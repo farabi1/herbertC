cd4d5bbd4d707684d192d7aba35e398555a37bf2
farabi1 <rashidfarabi@gmail.com>
2023-07-24 19:21:14 +0600

    chapter 1

--- Chapter 1/9Exercise/number3.c
+++ Chapter 1/9Exercise/number3.c
@@ -14,6 +14,6 @@ int f1(void)
 {
     return 100;
 }
-/*the program produce output result in float form but the value was initially returned from function as intiger , so in the main function the value converted from intiger to double form and ultimately produce the ans as 100.00000*/
+/*the program produce output result in float form but the value was initially returned from function as integer , so in the main function the value converted from integer to double form and ultimately produce the ans as 100.00000*/
 
 // clear
