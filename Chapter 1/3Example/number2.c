94dcced54f4ab6bf0a37c55e64949a865bb8d321
farabi1 <rashidfarabi@gmail.com>
2023-07-24 17:25:37 +0600

    chapter 1

---
+++ Chapter 1/3Example/number2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int count, num, val, total1, total2;
+    printf("Enter the number of count: ");
+    scanf("%d", &count);
+    printf("Enter the number of num: ");
+    scanf("%d", &num);
+    printf("Enter the number of val: ");
+    scanf("%d", &val);
+
+    total1 = count * num + 88 / val - 19 % count;
+    total2 = (count * num) + (88 / val) - (19 % count);
+    printf("The total1 is : %d \n", total1);
+    printf("The total2 is : %d", total2);
+    return 0;
+}
