8f2517b6b43ae3a31933b89441e47b747f934176
farabi1 <rashidfarabi@gmail.com>
2023-07-24 19:28:22 +0600

    chapter 1

--- Chapter 1/9Exercise/number4.c
+++ Chapter 1/9Exercise/number4.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+void func(void);
+
+int main(void)
+{
+    func();
+    return 0;
+}
+
+void func(void)
+{
+    int i;
+
+    printf("Enter a number : ");
+    scanf("%d", &i);
+    return i;
+}
+
+/*here the problem is the declared function named void func are actually returning a integer value so there is a conflict between declared value and returning value */
+
+// clear
