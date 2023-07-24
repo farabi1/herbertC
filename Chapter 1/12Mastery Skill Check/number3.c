3d80a6833087a34e4bf1d06658f6975ef176ce28
farabi1 <rashidfarabi@gmail.com>
2023-07-24 21:10:19 +0600

    chapter 1

--- Chapter 1/12Mastery Skill Check/number3.c
+++ Chapter 1/12Mastery Skill Check/number3.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+
+float o_to_c(void);
+
+int main(void)
+{
+    float numberOfCup = o_to_c();
+    printf("the number of cup are : %f", numberOfCup);
+
+    return 0;
+}
+
+float o_to_c(void)
+{
+    float ounce, cup;
+    printf("Enter the number of ounce : ");
+    scanf("%f", &ounce);
+    cup = (ounce / 8);
+    return cup;
+}
+
+// clear - Function ok-return type
