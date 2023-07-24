d7f87da6621f7f8a008180cb8192628fbbefea68
farabi1 <rashidfarabi@gmail.com>
2023-07-24 18:34:07 +0600

    chapter 1

--- Chapter 1/7Exercise/number2.c
+++ Chapter 1/7Exercise/number2.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+
+void function1(void);
+void function2(void);
+int main(void)
+{
+    function1();
+    function2();
+    return 0;
+}
+
+void function1(void)
+{
+    printf("The summer soldier, ");
+}
+
+void function2(void)
+{
+    printf("the sunshine patriot.");
+}
+
+// clear
