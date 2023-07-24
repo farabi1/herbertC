7f75904770ac023e80af8fa2ded2c98dc2ba7a4d
farabi1 <rashidfarabi@gmail.com>
2023-07-24 13:11:18 +0600

    herbertC

---
+++ Chapter 1/1Exercise/number1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int num;
+
+    num = 100;
+    printf("The value of the variable 'num' is: %d", num);
+    return 0;
+}
---
+++ Chapter 1/2Exercise/number2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    float f1, f2, sum;
+
+    printf("Enter first float : ");
+    scanf("%f", &f1);
+
+    printf("Enter second float : ");
+    scanf("%f", &f2);
+
+    sum = f1 + f2;
+
+    printf("The result is %f ", sum);
+
+    return 0;
+}
