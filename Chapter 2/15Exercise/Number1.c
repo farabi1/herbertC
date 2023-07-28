287b8b83a6026e3c76e5658d4837f2987ee3f186
farabi1 <rashidfarabi@gmail.com>
2023-07-28 22:46:32 +0600

    chapter 2

--- Chapter 2/15Exercise/Number1.c
+++ Chapter 2/15Exercise/Number1.c
@@ -1,1 +1,24 @@
-#
+#include <stdio.h>
+int main(void)
+{
+
+    int num1, num2, choice;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("Press 1: for sum \nPress 2: for product \n");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+        printf("The sum of the two number is : %d", num1 + num2);
+    if (choice == 2)
+        printf("The product of the two number is : %d", num1 * num2);
+    // else
+    //     printf("Not valid choice");
+
+    return 0;
+}
+
+// ekhane 1st if kaj korle er sathe else o kaj korbe, majher if bad jabe
