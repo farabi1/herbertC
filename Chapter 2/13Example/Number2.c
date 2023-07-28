571293b6bcbf6d5d7a583318894893cf8863d051
farabi1 <rashidfarabi@gmail.com>
2023-07-28 20:03:42 +0600

    chapter 2

--- Chapter 2/13Example/Number2.c
+++ Chapter 2/13Example/Number2.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+int main(void)
+{
+    float num;
+    int choice;
+
+    printf("Enter value : ");
+    scanf("%f", &num);
+
+    printf(" 1: Feet to Meters,\n 2: Meters to Feet. \n");
+    printf("Enter choice : ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+        printf("%f meter", num / 3.28);
+    if (choice == 2)
+        printf("%f feet", num * 3.28);
+
+    return 0;
+}
+// clear
