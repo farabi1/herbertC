b6df8766e727450c3a89a2a8a2c84e6bda262e12
farabi1 <rashidfarabi@gmail.com>
2023-07-28 19:53:10 +0600

    chapter 2

---
+++ Chapter 2/13Example/Number1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    int answer;
+    printf("What is 10 + 14?\n");
+    scanf("%d", &answer);
+    if (answer == (10 + 14))
+        printf("\nCorrect Answer");
+
+    return 0;
+}
+// clear
