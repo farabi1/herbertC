aa8526915f68d8fc2f473d77094379d099cdb15c
farabi1 <rashidfarabi@gmail.com>
2023-07-28 22:09:21 +0600

    chapter 2

--- Chapter 2/14Example/Number1.c
+++ Chapter 2/14Example/Number1.c
@@ -3,3 +3,20 @@
 // c. 10 * 9 < 90 is false
 // d. 1==1 is true
 // e. -1 is true
+
+// clear
+
+#include <stdio.h>
+int main()
+{
+
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num < 0)
+        printf("Number is negetive.");
+    else
+        printf("Number is non-negative");
+    return 0;
+}
+// clear
--- Chapter 2/14Example/Number2.c
+++ Chapter 2/14Example/Number2.c
@@ -1,15 +1,35 @@
+// #include <stdio.h>
+// int main()
+// {
+
+//     int number;
+//     printf("Enter a integer : ");
+//     scanf("%d", &number); /*Ekhane &number ke just number likhsilam*/
+//     if ((number % 2) == 0)
+//         printf("The number is even");
+//     if ((number % 2) != 0)
+//         printf("The number is odd");
+
+//     return 0;
+// }
+
+// clear
+
 #include <stdio.h>
-int main()
+int main(void)
 {
 
-    int number;
-    printf("Enter a integer : ");
-    scanf("%d", &number); /*Ekhane &number ke just number likhsilam*/
-    if ((number % 2) == 0)
-        printf("The number is even");
-    if ((number % 2) != 0)
-        printf("The number is odd");
+    int num1, num2;
+    printf("Enter first number : ");
+    scanf("%d", &num1);
 
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if (num2 == 0)
+        printf("Cannot divide by zero.");
+    else
+        printf("Answer is : %d", num1 / num2);
     return 0;
 }
 
