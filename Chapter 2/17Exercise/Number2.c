a7081b6d925f134da99b89873cae50e95161c8e7
farabi1 <rashidfarabi@gmail.com>
2023-07-29 00:05:10 +0600

    chapter 2

--- Chapter 2/17Exercise/Number2.c
+++ Chapter 2/17Exercise/Number2.c
@@ -1,31 +1,9 @@
-#include <stdio.h>
-int main(void)
-{
+// the fragment of code is incorrect
 
-    int num1, num2, ops;
-    printf("Enter the operation : \n1: add\n2: substract\n");
-    scanf("%d", &ops);
-    if (ops == 1)
-    {
-        printf("Enter the first number : ");
-        scanf("%d", &num1);
+// if(count < 100)printf("Number is less than 100.");
+// printf("Its square is %d.",count*count);
+// }
 
-        printf("Enter the second number : ");
-        scanf("%d", &num2);
+// it does not have first curly brace for the block of code after if condition
 
-        printf("The sum of the numbers is : %d", num1 + num2);
-    }
-    if (ops == 2)
-    {
-        printf("Enter the first number : ");
-        scanf("%d", &num1);
-
-        printf("Enter the second number : ");
-        scanf("%d", &num2);
-
-        printf("The subtract of the numbers is : %d", num1 - num2);
-    }
-
-    return 0;
-}
 // clear
