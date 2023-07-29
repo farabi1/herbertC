6173a5b84f4faf036ead19d338e338f2ad8fab07
farabi1 <rashidfarabi@gmail.com>
2023-07-29 15:05:27 +0600

    Chapter 2

--- Chapter 2/18Example/Number2.c
+++ Chapter 2/18Example/Number2.c
@@ -7,7 +7,8 @@ int main(void)
     /*now test for factor*/
 
     is_prime = 1;
-    for (i = 2; i <= num / 2; i++)
+
+    for (i = 2; i <= num; i++)
         if ((num % i == 0))
             is_prime = 0;
 
