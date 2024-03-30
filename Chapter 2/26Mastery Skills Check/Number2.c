aafe2f1980c051fbffd940e516a4ab00e938aefe
farabi1 <rashidfarabi@gmail.com>
2024-03-30 16:36:17 +0600

    chapter 2

---
+++ Chapter 2/26Mastery Skills Check/Number2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    
+    int length,width,total=0,room_no,dimension;
+    printf("Enter the number of room : ");
+    scanf("%d",&room_no);
+    for(int i=1;i<=room_no;i++){
+        printf("Enter the length of room %d :",i);
+        scanf("%d",&length);
+        printf("Enter the width of room %d :",i);
+        scanf("%d",&width);
+        dimension=length*width;
+        total=total+dimension;
+        
+
+
+    }
+
+    printf("Total square footage is %d \n",total);
+    
+    return 0;
+}
+
+//for loop er vitorer variable baire theke access kora jai
+// clear
