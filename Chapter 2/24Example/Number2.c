fc819edee91eb40b23f0ca52a0e0c26f9f1a5047
farabi1 <rashidfarabi@gmail.com>
2024-03-30 12:16:27 +0600

    chapter 2

---
+++ Chapter 2/24Example/Number2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+
+    printf("Enter the first number : ");
+    scanf("%d",&i);
+    printf("Enter the second number : ");
+    scanf("%d",&j);
+
+    /* Relational Operations*/
+
+    printf("i<j %d \n",i<j);
+    printf("i<=j %d \n",i<=j);
+    printf("i==j %d \n",i==j);
+    printf("i>j %d \n",i>j);
+    printf("i>=j %d \n",i>=j);
+
+    /*Logical Operations*/
+
+    printf("i && j %d \n",i && j);
+    printf("i || j %d \n",i ||j);
+    printf("!i !j %d %d \n",!i,!j);
+    
+    return 0;
+}
