dc72c41e99a5385db243f7af2f5f18db8222803e
farabi1 <rashidfarabi@gmail.com>
2024-03-30 13:14:40 +0600

    chapter 2

---
+++ Chapter 2/25Exercise/Number4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    printf("1 && 1 is %d \n",1 && 1);
+    printf("1 || 1 is %d \n",1 || 1);
+    printf("1 || 0 is %d \n",1 || 0);
+    printf("1 && 0 is %d \n",1 && 0);
+    printf("1 != 1 is %d \n",(1 != 1));
+    printf("1 != 0 is %d \n",(1 != 0));
+    printf("1 == 1 is %d \n",1 == 1);
+    printf("1 == 0 is %d",1 == 0);
+
+
+
+    return 0;
+}
+//clear
