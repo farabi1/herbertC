c3c420c7c12df12357efec3da9a8ab8924132508
farabi1 <rashidfarabi@gmail.com>
2024-03-31 12:36:57 +0600

    chapter 3

---
+++ Chapter 3/28Example/Number1.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+    printf("Enter a character : ");
+    ch=getche();
+    printf("\n Its ASCII  code is %d",ch);
+
+
+    return 0;
+}
+
+//clear
