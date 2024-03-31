dd38b98cc47944e8fe588a82a18e55b321bc83ce
farabi1 <rashidfarabi@gmail.com>
2024-03-31 16:20:10 +0600

    chapter 3

--- Chapter 3/29Exercise/Number2.c
+++ Chapter 3/29Exercise/Number2.c
@@ -1,1 +1,29 @@
- 
+// #include<stdio.h>
+// int main(){
+
+//     char ch;
+//     printf("Enter kthe character :");
+//     ch=getchar();
+//     getchar();
+//     printf("The ASCII Code for the entered character is %d",ch);
+//     return 0;
+// }
+
+
+
+#include<stdio.h>
+int main(){
+
+    // for A through Z
+    for (char c='A';c<='Z';c++){
+        printf("%c %d \n",c,c);
+    }
+
+    // for a through z
+
+    for(char c='a';c<='z';c++){
+        printf("%c %d \n",c,c);
+    }
+
+    return 0;
+}
