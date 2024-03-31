2311a76b6d7fd55d8969b1b80dca36ac09711416
farabi1 <rashidfarabi@gmail.com>
2024-03-31 11:58:08 +0600

    chapter 3

---
+++ Chapter 3/27Review Skill Check/Number5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    
+    int num;
+    printf("Enter the number of proverbs :");
+    scanf("%d",&num);  
+ 
+        if(num>=1)printf("1. A stitch in time saves nine.\n");
+        if(num>=2)printf("2. Actions speak louder than words.\n\n");
+        if(num>=3)printf("3. Don't count your chickens before they hatch.\n");
+        if(num>=4)printf("4. All good things come to those who wait.\n");
+        if(num>=5)printf("5. The early bird catches the worm.\n");
+    
+    
+    return 0;
+
+}
+
+//clear
